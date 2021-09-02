5050420657d03784962f7071bee3253b793a04f8
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-09-01 23:17:58 -0700

    clean app.js, move all into porto folder

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import { useState } from "react"
+import Porto from "./components/porto/Porto"
 import "./styles/App.css"
-import items from "./allData"
-import Menu from "./components/Menu"
-import Button from "./components/Button"
-
-const allCategories = ["All", ...new Set(items.map((item) => item.category))]
 
 function App() {
-  const [menuItem, setMenuItem] = useState(items)
-  const [buttons, setButtons] = useState(allCategories)
-
-  const filter = (button) => {
-    if (button === "All") {
-      setMenuItem(items)
-      return
-    }
-
-    const filteredData = items.filter((item) => item.category === button)
-    setMenuItem(filteredData)
-  }
-
   return (
     <div className="App">
-      <div className="title">
-        <h1>
-          Portofolio
-          <span> filter</span>
-        </h1>
-      </div>
-      <Button button={buttons} filter={filter} />
-      <Menu menuItem={menuItem} />
+      <Porto />
     </div>
   )
 }
